8945dbe644ab8f927daa98bacc09ebea3567fac4
TbobpenguinE <Lansingh.Corin@student.greenriver.edu>
2025-11-26 11:30:35 -0800

    Done

--- potato.js
+++ potato.js
@@ -1,1 +1,16 @@
 // Add your javascript in this file
+
+function togglePotatoScheme() {
+    document.body.classList.toggle("potato-scheme");
+}  
+
+
+// Toggle the hidden potato fact div
+function revealPotatoFact(){
+    document.getElementById("faq-answer").classList.toggle("hidden");
+}
+
+// font size for the whole page
+function textsize() {
+    document.body.classList.toggle("sizeup");
+}
